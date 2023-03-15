7529d302aa7294a986291dbda20348c2dbb5fefb
kitti-katy <kitti-katy@mail.ru>
2023-03-15 14:53:52 -0400

    bring back OperationalStaking upgrade name"

--- scripts/staking-multisig-upgrade.js
+++ scripts/staking-multisig-upgrade.js
@@ -3,7 +3,7 @@ const { ethers } = require('hardhat');
 const stakingAddress = process.env.STAKING_ADDRESS
 
 async function main() {
-    const OperationalStakingV2 = await ethers.getContractFactory("MigrationOperationalStaking");
+    const OperationalStakingV2 = await ethers.getContractFactory("OperationalStaking");
     console.log("Preparing proposal...");
     const proposal = await defender.proposeUpgrade(stakingAddress, OperationalStakingV2);
     console.log("Upgrade proposal created at:", proposal.url);
