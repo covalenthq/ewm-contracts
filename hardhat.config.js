592c1a38b1c1e39223c3a324e4f402da542f20c3
Rodrigo Garcia <rodrigo@covalenthq.com>
2024-05-28 00:55:17 -0700

    checkpoint 3: fix coverage task

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,7 @@
 require('@openzeppelin/hardhat-upgrades');
 require('hardhat-abi-exporter');
 require('hardhat-contract-sizer');
+require('solidity-coverage');
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
